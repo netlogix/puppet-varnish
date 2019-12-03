719cc3b60033bc2bc14c000373f308977f49dbdf
Christian Klein <christian.klein@netlogix.de>
2019-12-03 16:42:01 +0100

    [CLEANUP] Change indention from spaces to tabs

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,58 +3,58 @@
 
 class varnish::params {
 
-  # set Varnish conf location based on OS
-  case $::osfamily {
-    'RedHat': {
-      $default_version = '3.0'
-      $add_repo = true
-      $vcl_reload_script = '/usr/sbin/varnish_reload_vcl'
-      if ($::service_provider == 'systemd') {
-        $systemd = true
-        $systemd_conf_path = '/usr/lib/systemd/system/varnish.service'
-      } else {
-        $systemd = false
-      }
-      if versioncmp($::operatingsystemmajrelease, '7') >= 0 {
-        $conf_file_path = '/etc/varnish/varnish.params'
-      } else {
-        $conf_file_path = '/etc/sysconfig/varnish'
-      }
-    }
-    'Debian': {
-	    if versioncmp($::operatingsystemmajrelease, '18.04') < 0 {
+	# set Varnish conf location based on OS
+	case $::osfamily {
+		'RedHat': {
+			$default_version = '3.0'
+			$add_repo = true
+			$vcl_reload_script = '/usr/sbin/varnish_reload_vcl'
+			if ($::service_provider == 'systemd') {
+				$systemd = true
+				$systemd_conf_path = '/usr/lib/systemd/system/varnish.service'
+			} else {
+				$systemd = false
+			}
+			if versioncmp($::operatingsystemmajrelease, '7') >= 0 {
+				$conf_file_path = '/etc/varnish/varnish.params'
+			} else {
+				$conf_file_path = '/etc/sysconfig/varnish'
+			}
+		}
+		'Debian': {
+			if versioncmp($::operatingsystemmajrelease, '18.04') < 0 {
 				$vcl_reload_script = '/usr/share/varnish/reload-vcl'
-      } else {
+			} else {
 				$vcl_reload_script = '/usr/share/varnish/varnishreload'
-	    }
-      if ($::service_provider == 'systemd' or
-          ($::operatingsystem == 'Ubuntu' and 
-          versioncmp($::operatingsystemmajrelease, '15.10') > 0)) {
-        $systemd = true
-        $systemd_conf_path = '/etc/systemd/system/varnish.service'
-        $systemd_ncsa_conf_path = '/etc/systemd/system/varnishncsa.service'
-        $conf_file_path = '/etc/varnish/varnish.params'
-      } else {
-        $systemd = false
-        $conf_file_path = '/etc/default/varnish'
-      }
-      if ($::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemmajrelease, '15.10') > 0) {
-        #don't add repo as in default repo
-        $add_repo = false
-        $default_version ='4.1'
-      } elsif ($::operatingsystem == 'Debian' and versioncmp($::operatingsystemmajrelease, '7') > 0) {
-        #don't add repo as in default repo
-        $add_repo = false
-        $default_version ='4.1'
-      }
-      else {
-        $add_repo = true
-        $default_version = '3.0'
+			}
+			if ($::service_provider == 'systemd' or
+					($::operatingsystem == 'Ubuntu' and 
+					versioncmp($::operatingsystemmajrelease, '15.10') > 0)) {
+				$systemd = true
+				$systemd_conf_path = '/etc/systemd/system/varnish.service'
+				$systemd_ncsa_conf_path = '/etc/systemd/system/varnishncsa.service'
+				$conf_file_path = '/etc/varnish/varnish.params'
+			} else {
+				$systemd = false
+				$conf_file_path = '/etc/default/varnish'
+			}
+			if ($::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemmajrelease, '15.10') > 0) {
+				#don't add repo as in default repo
+				$add_repo = false
+				$default_version ='4.1'
+			} elsif ($::operatingsystem == 'Debian' and versioncmp($::operatingsystemmajrelease, '7') > 0) {
+				#don't add repo as in default repo
+				$add_repo = false
+				$default_version ='4.1'
+			}
+			else {
+				$add_repo = true
+				$default_version = '3.0'
 
-      }
-    }
-    default: {
-      fail("Class['apache::params']: Unsupported osfamily: ${::osfamily}")
-    }
-  }
+			}
+		}
+		default: {
+			fail("Class['apache::params']: Unsupported osfamily: ${::osfamily}")
+		}
+	}
 }
